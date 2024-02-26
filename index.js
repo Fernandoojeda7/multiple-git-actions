f2d4a85786a80a3770e2e202a99baf74a3ac4774
Fernando Ojeda <fernandoojeda7@gmail.com>
2024-02-26 18:03:25 -0300

    Subiendo la app

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+app.use(morgan("dev"));
+app.get("/", (req, res) => {
+  console.log("Request: Hello Equipzilla!");
+  res.send("Hello Equipzilla, Good Morning!");
+});
+
+app.listen(3001, () => {
+  console.log("Listening on port 3001!");
+});
